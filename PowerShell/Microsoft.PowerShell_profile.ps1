ef90d25558a52972f77d3460615906ea3cd31425
maxxnino <idonthaveemail@dontknow.com>
2021-09-12 11:36:55 +0900

    alias for lunarvim

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
-oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh3)\themes\custom_gmay.omp.json" | Invoke-Expression
+oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh3)/themes/custom_gmay.omp.json" | Invoke-Expression
 Set-Alias -Name v -Value "lvim.ps1"
+Set-Variable -name "LUNARVIM_RUNTIME_DIR" -Value "$HOME/.local/share/lunarvim"
+Set-Variable -name "LUNARVIM_CONFIG_DIR" -Value "$HOME/.config/lvim"
