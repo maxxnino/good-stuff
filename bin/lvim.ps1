206b5c32e74ba5b42d686a5fbe65c99d92de65bb
maxxnino <idonthaveemail@dontknow.com>
2021-09-12 11:36:04 +0900

    lunarvim open script

--- bin/lvim.ps1
+++ bin/lvim.ps1
@@ -1,5 +1,8 @@
+$env:HOME = "$HOME"
+$env:USER = "$HOME"
+$env:SHELL = "cmd"
+$env:TERMINAL = "$env:SHELL"
+$env:VIMRUNTIME = "$(scoop prefix neovim-nightly)/share/nvim/runtime";
 Set-Variable -name "LUNARVIM_RUNTIME_DIR" -Value "$HOME/.local/share/lunarvim"
 Set-Variable -name "LUNARVIM_CONFIG_DIR" -Value "$HOME/.config/lvim"
-#Write-Output $LUNARVIM_RUNTIME_DIR
-#Write-Output $LUNARVIM_CONFIG_DIR
 nvim -u "$LUNARVIM_RUNTIME_DIR/lvim/init.lua" $args
