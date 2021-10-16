4914d555dc74c1c6c1e4f4807215449cc72cad91
maxxnino <34153891+maxxnino@users.noreply.github.com>
2021-10-16 22:45:58 +0900

    vim mode in pwsh and show history when typing

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,30 @@
-oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh3)/themes/custom_gmay.omp.json" | Invoke-Expression
+# set power shell theme
+oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh3)/themes.original/amro.omp.json" | Invoke-Expression
+
+#lunar vim alias
 Set-Alias -Name v -Value "lvim.ps1"
-Set-Variable -name "LUNARVIM_RUNTIME_DIR" -Value "$HOME/.local/share/lunarvim"
-Set-Variable -name "LUNARVIM_CONFIG_DIR" -Value "$HOME/.config/lvim"
+
+# vim mode
+$env:VISUAL = "nvim"
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+#change curso in insert/normal mode
+function OnViModeChange {
+	if ($args[0] -eq 'Command') {
+		# Set the cursor to a blinking block.
+		Write-Host -NoNewLine "`e[1 q"
+	} else {
+		# Set the cursor to a blinking line.
+		Write-Host -NoNewLine "`e[5 q"
+	}
+}
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $function:OnViModeChange
+
+Set-PSReadLineKeyHandler -Key Ctrl+j -Function NextHistory
+Set-PSReadLineKeyHandler -Key Ctrl+k -Function PreviousHistory
+
+
+
