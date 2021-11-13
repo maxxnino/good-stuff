d628e049958e974085af234239eb83aedb257133
maxxnino <34153891+maxxnino@users.noreply.github.com>
2021-11-14 00:20:03 +0900

    powershell profile

--- config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -8,34 +8,11 @@ Set-Alias -Name v -Value "nvim"
 $env:VISUAL = "nvim"
 Set-PSReadLineOption -EditMode Vi
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineOption -PredictionSource History
-
-# TODO: enable with pwsh 7.2
-#Set-PSReadLineOption -PredictionSource HistoryAndPlugin 
-
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin 
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-#change curso in insert/normal mode
-function OnViModeChange {
-	if ($args[0] -eq 'Command') {
-		# Set the cursor to a blinking block.
-		Write-Host -NoNewLine "`e[1 q"
-	} else {
-		# Set the cursor to a blinking line.
-		Write-Host -NoNewLine "`e[5 q"
-	}
-}
-Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $function:OnViModeChange
-
-Set-PSReadLineKeyHandler -Key Ctrl+j -Function NextHistory
-Set-PSReadLineKeyHandler -Key Ctrl+k -Function PreviousHistory
+Set-PSReadLineKeyHandler -Key Ctrl+n -Function NextHistory
+Set-PSReadLineKeyHandler -Key Ctrl+p -Function PreviousHistory
 
 #vulkan validation layer
 $env:VK_LAYER_PATH= "$(scoop prefix vulkan)/bin"
-
-#zig build self-hosted compiler
-Set-Alias -Name zb -Value "zig-dev-build.ps1"
-
-
-
-
